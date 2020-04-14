f168784727443243385ab974d445805cbcb422f1
Estuardo López <estuardo.lopez.hernandez@ericsson.com>
2020-04-13 18:38:47 -0600

    no antialiasing; no rgb channels; no extended vertical draw; column resized; rbg to hexa

--- image2excel.py
+++ image2excel.py
@@ -3,23 +3,14 @@ import xlsxwriter
 from PIL import Image
 import sys
 
-def toHexa(val, channel):
-    """
-    Convert decimal value to hexadecimal color code where
-    only channel given has a value and the other two are set
-    to 00.
-
-    val: decimal value
-    channel: color channel
-    """
-
-    if channel == 'r':  
-        return '#' + '{:02x}'.format(val) + '0000'
-    elif channel == 'g':
-        return '#00' + '{:02x}'.format(val) + '00'
+def componentToHex(component):
+    if len(str(hex(component)[2:])) == 1:
+      return '0' + hex(component)[2:]
     else:
-        return '#0000' + '{:02x}'.format(val)
+      return hex(component)[2:]
 
+def rgbToHexa(rval, gval, bval):
+    return '#' + componentToHex(rval) + '' + componentToHex(gval) + '' + componentToHex(bval)
 
 def main():
 
@@ -28,29 +19,27 @@ def main():
     # load image
     im = Image.open(sys.argv[1])
     imsize = im.size
-    print(f'Input image size: ({imsize[0]}, {imsize[1]})')
     if imsize[0] > 128 or imsize[1] > 128:
-        print('Downsampling image.')
-        im.thumbnail((128,128), Image.ANTIALIAS) # downsampling, aspect ratio stays the same
+        im.thumbnail((128,128), Image.ALIAS) # downsampling, aspect ratio stays the same
         imsize = im.size
-        print(f'Image size after downsampling: ({imsize[0]}, {imsize[1]})')
     pix = im.load()
-    
+
     # create excel workbook/worksheet
     workbook = xlsxwriter.Workbook( './output.xls')
     worksheet = workbook.add_worksheet('image')
 
+    worksheet.set_column(0, imsize[0], 3);
+
     # color excel cells
     for x in range(imsize[0]):
-        for y in range(0, imsize[1]*3, 3):
-            colors = pix[x, y/3]
-            for i,channel in enumerate(['r','g','b']):
-                hexcode = toHexa(colors[i], channel=channel)
-                wbformat = workbook.add_format({'bg_color':hexcode})
-                worksheet.write(y+i,x,colors[i], wbformat)
+        for y in range(imsize[1]):
+            colors = pix[x, y]
+            hexcode = rgbToHexa(colors[0], colors[1], colors[2])
+            print str(hexcode)
+            wbformat = workbook.add_format({'bg_color':hexcode})
+            worksheet.write(y,x,'', wbformat)
 
     workbook.close()
-    print('Spreadsheet complete. Saved to ./output.xls')
 
 if __name__ == '__main__':
     main()
